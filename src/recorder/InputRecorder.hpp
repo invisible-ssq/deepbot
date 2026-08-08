4df2c82e1fbd97093593c2ad31347ec0605c4856
invisible-ssq <ilu060516@gmail.com>
2026-08-08 16:15:52 +0300

    Update InputRecorder.hpp

--- src/recorder/InputRecorder.hpp
+++ src/recorder/InputRecorder.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <vector>
 #include <cstdint>
-#include <Geode/Geode.hpp>
+#include <array>
 #include "../utils/TPSFix.hpp"
 
 namespace deepbot {
@@ -10,19 +10,18 @@ class InputRecorder {
 private:
     bool m_recording = false;
     std::vector<TPSIndependentFrame> m_frames;
-    double m_startTime = 0.0;
     double m_currentTPS = 240.0;
     uint32_t m_seed = 0;
-    bool m_lastP1Down = false;
-    bool m_lastP2Down = false;
+    std::array<bool, 4> m_lastP1Down = {false, false, false, false};
+    std::array<bool, 4> m_lastP2Down = {false, false, false, false};
 
 public:
     void startRecording(double tps, uint32_t seed);
     void stopRecording();
     bool isRecording() const { return m_recording; }
     void recordInput(bool down, bool player2, uint8_t button,
-                     float x, float y, float rot, float yAccel);
-    std::vector<TPSIndependentFrame> getFrames() const { return m_frames; }
+        float x, float y, float rot, float yAccel);
+    const std::vector<TPSIndependentFrame>& getFrames() const { return m_frames; }
     void clear() { m_frames.clear(); }
     double getDuration() const;
     void setTPS(double tps) { m_currentTPS = tps; }
