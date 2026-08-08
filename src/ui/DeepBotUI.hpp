a7df59d8f461ad6ec6ff8d1015d88681bece4f4b
invisible-ssq <ilu060516@gmail.com>
2026-08-08 21:16:52 +0300

    Update DeepBotUI.hpp

--- src/ui/DeepBotUI.hpp
+++ src/ui/DeepBotUI.hpp
@@ -5,31 +5,74 @@ using namespace geode::prelude;
 
 namespace deepbot {
 
-class DeepBotUI : public FLAlertLayer {
-protected:
-    bool init() override;
+enum class DeepBotTab {
+    Macro,
+    Settings
+};
 
+class DeepBotUI : public CCLayerColor {
 public:
     static DeepBotUI* create();
-    void show() override;
-    void onClose(CCObject*);
+    bool init() override;
+    void show();
+    void hide();
     void updateStatus(const std::string& status);
-    void refreshButtons();
 
 private:
+    CCMenu* m_tabMenu = nullptr;
+    CCMenu* m_macroMenu = nullptr;
+    CCMenu* m_settingsMenu = nullptr;
     CCLabelBMFont* m_statusLabel = nullptr;
     CCMenuItemSpriteExtra* m_recordBtn = nullptr;
     CCMenuItemSpriteExtra* m_stopBtn = nullptr;
-    CCMenuItemSpriteExtra* m_playBtn = nullptr;
-
+    
+    DeepBotTab m_currentTab = DeepBotTab::Macro;
+    
+    void onTabMacro(CCObject*);
+    void onTabSettings(CCObject*);
     void onRecord(CCObject*);
     void onStop(CCObject*);
     void onPlay(CCObject*);
     void onSave(CCObject*);
     void onLoad(CCObject*);
-    void onConvert(CCObject*);
-    void onSaveFormat(CCObject*);
-    void onSaveCustomFormat(CCObject*);
+    void onClose(CCObject*);
+    
+    void switchTab(DeepBotTab tab);
+    void refreshButtons();
+};
+
+class DeepBotLoadLayer : public CCLayerColor {
+public:
+    static DeepBotLoadLayer* create();
+    bool init() override;
+    void show();
+    void hide();
+    
+private:
+    void refreshList();
+    void onLoadMacro(CCObject*);
+    void onDeleteMacro(CCObject*);
+    void onClose(CCObject*);
+};
+
+class DeepBotSaveLayer : public CCLayerColor {
+public:
+    static DeepBotSaveLayer* create();
+    bool init() override;
+    void show();
+    void hide();
+    
+private:
+    int m_selectedFormat = 0; // 0=deep, 1=ttr3, 2=gdr, 3=gdr2, 4=slc, 5=xd, 6=ybot, 7=txt
+    std::string m_name;
+    std::string m_author;
+    std::string m_desc;
+    
+    void onFormatSelect(CCObject*);
+    void onSaveConfirm(CCObject*);
+    void onOtherFormat(CCObject*);
+    void onClose(CCObject*);
+    void refreshFormatButtons();
 };
 
 } // namespace deepbot
