526bd69b0d66d6e66db1c6af7147bccc69eeecd4
invisible-ssq <ilu060516@gmail.com>
2026-08-08 21:49:03 +0300

    Update InputRecorder.cpp

--- src/recorder/InputRecorder.cpp
+++ src/recorder/InputRecorder.cpp
@@ -1,7 +1,5 @@
 #include "InputRecorder.hpp"
-#include "../DeepBot.hpp"
-#include <Geode/modify/PlayLayer.hpp>
-#include <array>
+#include <Geode/Geode.hpp>
 
 using namespace geode::prelude;
 
@@ -9,80 +7,44 @@ namespace deepbot {
 
 void InputRecorder::startRecording(double tps, uint32_t seed) {
     m_recording = true;
-    m_frames.clear();
-    m_currentTPS = tps;
+    m_tps = tps;
     m_seed = seed;
-    m_lastP1Down.fill(false);
-    m_lastP2Down.fill(false);
+    m_frames.clear();
+    m_startTime = getCurrentTime();
 }
 
 void InputRecorder::stopRecording() {
     m_recording = false;
 }
 
-void InputRecorder::recordInput(bool down, bool player2, uint8_t button,
-    float x, float y, float rot, float yAccel) {
+double InputRecorder::getCurrentTime() {
+    return CCDirector::sharedDirector()->getAnimationInterval() * 
+           CCDirector::sharedDirector()->getTotalFrames();
+}
 
+void InputRecorder::recordInput(bool down, bool player2, int button) {
     if (!m_recording) return;
-
-    if (button == 0 || button > 3) button = 1;
-
-    auto& lastDown = player2 ? m_lastP2Down : m_lastP1Down;
-    if (button < lastDown.size() && down == lastDown[button]) {
-        return;
-    }
-
-    auto* playLayer = PlayLayer::get();
-    if (!playLayer) return;
-
-    double time = 0.0;
-    #if defined(GEODE_IS_WINDOWS)
-        time = playLayer->m_gameState.m_levelTime;
-    #else
-        time = playLayer->m_gameState.m_levelTime;
-    #endif
-
-    TPSIndependentFrame frame;
-    frame.absoluteTime = time;
+    
+    RecordedFrame frame;
+    frame.absoluteTime = getCurrentTime() - m_startTime;
     frame.down = down;
     frame.player2 = player2;
     frame.button = button;
-    frame.x = x;
-    frame.y = y;
-    frame.rotation = rot;
-    frame.yAccel = yAccel;
+    frame.x = 0;
+    frame.y = 0;
+    frame.rotation = 0;
+    frame.yAccel = 0;
     m_frames.push_back(frame);
-
-    if (button < lastDown.size()) {
-        lastDown[button] = down;
-    }
 }
 
-double InputRecorder::getDuration() const {
-    if (m_frames.empty()) return 0.0;
-    return m_frames.back().absoluteTime;
+void InputRecorder::recordFrame(float x, float y, float rotation, float yAccel) {
+    if (!m_recording || m_frames.empty()) return;
+    
+    auto& last = m_frames.back();
+    last.x = x;
+    last.y = y;
+    last.rotation = rotation;
+    last.yAccel = yAccel;
 }
-
-class $modify(PlayLayerRecorder, PlayLayer) {
-    void handleButton(bool down, int button, bool player2) {
-        PlayLayer::handleButton(down, button, player2);
-        auto& recorder = deepbot::DeepBot::instance().getRecorder();
-        if (!recorder.isRecording()) return;
-
-        uint8_t btn = 1;
-        if (button == 2) btn = 2;
-        if (button == 3) btn = 3;
-
-        float x = 0, y = 0, rot = 0, yAccel = 0;
-        auto* player = player2 ? m_player2 : m_player1;
-        if (player) {
-            x = player->m_position.x;
-            y = player->m_position.y;
-            rot = player->getRotation();
-            yAccel = player->m_yVelocity;
-        }
-        recorder.recordInput(down, player2, btn, x, y, rot, yAccel);
-    }
-};
 
 } // namespace deepbot
