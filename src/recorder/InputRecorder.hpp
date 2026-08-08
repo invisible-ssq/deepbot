773136670876095a3425040be318e7f63481b282
invisible-ssq <ilu060516@gmail.com>
2026-08-08 21:48:52 +0300

    Update InputRecorder.hpp

--- src/recorder/InputRecorder.hpp
+++ src/recorder/InputRecorder.hpp
@@ -1,30 +1,36 @@
 #pragma once
 #include <vector>
 #include <cstdint>
-#include <array>
 #include "../utils/TPSFix.hpp"
 
 namespace deepbot {
 
 class InputRecorder {
-private:
-    bool m_recording = false;
-    std::vector<TPSIndependentFrame> m_frames;
-    double m_currentTPS = 240.0;
-    uint32_t m_seed = 0;
-    std::array<bool, 4> m_lastP1Down = {false, false, false, false};
-    std::array<bool, 4> m_lastP2Down = {false, false, false, false};
-
 public:
+    struct RecordedFrame {
+        double absoluteTime;
+        bool down;
+        bool player2;
+        int button;
+        float x, y;
+        float rotation;
+        float yAccel;
+    };
+
     void startRecording(double tps, uint32_t seed);
     void stopRecording();
     bool isRecording() const { return m_recording; }
-    void recordInput(bool down, bool player2, uint8_t button,
-        float x, float y, float rot, float yAccel);
-    const std::vector<TPSIndependentFrame>& getFrames() const { return m_frames; }
-    void clear() { m_frames.clear(); }
-    double getDuration() const;
-    void setTPS(double tps) { m_currentTPS = tps; }
+    void recordInput(bool down, bool player2, int button);
+    void recordFrame(float x, float y, float rotation, float yAccel);
+    const std::vector<RecordedFrame>& getFrames() const { return m_frames; }
+
+private:
+    bool m_recording = false;
+    double m_tps = 240.0;
+    double m_startTime = 0.0;
+    uint32_t m_seed = 0;
+    std::vector<RecordedFrame> m_frames;
+    double getCurrentTime();
 };
 
 } // namespace deepbot
