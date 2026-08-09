e08c8a6c8e4bfe6b10fc1d9fb4b96773c87fd948
invisible-ssq <ilu060516@gmail.com>
2026-08-09 11:22:16 +0300

    Update DeepBotUI.hpp

--- src/ui/DeepBotUI.hpp
+++ src/ui/DeepBotUI.hpp
@@ -11,26 +11,19 @@ public:
     bool init() override;
     void show();
     void hide();
-    void updateStatus(const std::string& status);
 
 private:
-    CCMenu* m_tabMenu = nullptr;
     CCMenu* m_macroMenu = nullptr;
-    CCMenu* m_settingsMenu = nullptr;
     CCLabelBMFont* m_actionsLabel = nullptr;
     CCMenuItemSpriteExtra* m_recordBtn = nullptr;
     CCMenuItemSpriteExtra* m_stopBtn = nullptr;
     
-    void onTabMacro(CCObject*);
-    void onTabSettings(CCObject*);
     void onRecord(CCObject*);
     void onStop(CCObject*);
     void onPlay(CCObject*);
     void onSave(CCObject*);
     void onLoad(CCObject*);
     void onClose(CCObject*);
-    
-    void switchTab(int tab);
     void refreshButtons();
 };
 
@@ -56,20 +49,13 @@ public:
     void hide();
     
 private:
-    int m_selectedFormat = 0;
     CCTextInputNode* m_nameInput = nullptr;
-    CCTextInputNode* m_authorInput = nullptr;
-    CCTextInputNode* m_descInput = nullptr;
     CCTextInputNode* m_otherFormatInput = nullptr;
-    CCMenu* m_formatMenu = nullptr;
     CCMenu* m_otherMenu = nullptr;
     
-    void onFormatSelect(CCObject*);
     void onSaveConfirm(CCObject*);
     void onOtherFormat(CCObject*);
     void onClose(CCObject*);
-    void showOtherInput();
-    void hideOtherInput();
 };
 
 } // namespace deepbot
